bafc8f43abf4af98434bbc838504e689b1993f48
Hamza Khalid <hamza.khalid96804@gmail.com>
2024-04-01 02:49:31 +0500

    Feat: Added Counter for 1 Second

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [showCounter, setShowCounter] = useState(false);
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (showCounter) {
+      intervalId = setInterval(() => {
+        setCount((prevCount) => prevCount + 1.0);
+      }, 1000);
+    } else {
+      console.log(`interval before clear ${intervalId}`);
+      clearInterval(intervalId);
+      console.log(`interval after clear ${intervalId}`);
+    }
+
+    console.log(`Just before return ${intervalId}`);
+
+    return () => clearInterval(intervalId);
+  }, [showCounter]);
+
+  const handleShowCounter = () => {
+    setShowCounter(true);
+  };
+
+  const handleHideCounter = () => {
+    setShowCounter(false);
+    setCount(0);
+  };
+
+  return (
+    <div className="container">
+      <h2>{showCounter && <div>Counter: {count}</div>}</h2>
+      <button onClick={handleShowCounter}>Show</button>
+      <button onClick={handleHideCounter}>Hide</button>
+    </div>
+  );
+}
+
+export default App;
